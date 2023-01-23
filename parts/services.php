2a4e93fc6d282ea3285f32bf74262f695fb0b7f8
ChristianBinkert <99551402+ChristianBinkert@users.noreply.github.com>
2023-01-23 08:44:25 +0100

    Update service text

--- parts/services.php
+++ parts/services.php
@@ -1,31 +1,31 @@
 <section class="services" id="services">
   <div class="container">
-    <h3 class="text-center">Dienstleistungen</h3>
-    <p class="text-center text-muted font-italic mb-5">Wir bieten tolle Dienstleistungen an.</p>
+    <h3 class="text-center">Aktivitäten</h3>
+    <p class="text-center text-muted font-italic mb-5">Verbringen Sie abwechslungsreiche Tage in der Toskana Deutschlands.</p>
     <div class="row">
       <div class="col-sm">
         <div class="services-icon">
           <i class="fas fa-shopping-basket"></i>
         </div>
 
-        <h4 class="text-center">E-Commerce</h4>
-        <p class="text-center text-muted font-italic">Wir entwickeln schicke Online-Shops, die Ihre Produkte gut verkaufen. So erzielen Sie top Umsätze.</p>
+        <h4 class="text-center">Wandern im Schwarzwald</h4>
+        <p class="text-center text-muted font-italic">Wandern Sie im nahegelegenen Schwarzwald über den Feldberg oder Kandel.</p>
       </div>
       <div class="col-sm">
         <div class="services-icon">
           <i class="fas fa-laptop-code"></i>
         </div>
 
-        <h4 class="text-center">Responsive Web</h4>
-        <p class="text-center text-muted font-italic">Unsere Webseiten sehen überall gut aus: Egal ob auf dem Tablet, Desktop oder Handy.</p>
+        <h4 class="text-center">Europapark</h4>
+        <p class="text-center text-muted font-italic">Der größten Freizeitpark Deutschlands bietet Spaß für die ganze Familie.</p>
       </div>
       <div class="col-sm">
         <div class="services-icon">
           <i class="fas fa-lock"></i>
         </div>
 
-        <h4 class="text-center">Sicherheit</h4>
-        <p class="text-center text-muted font-italic">Auf Sicherheit legen wir viel Wert. So sind Ihre Daten und die Ihrer Kunden gut geschützt.</p>
+        <h4 class="text-center">Freiburg erkundnen</h4>
+        <p class="text-center text-muted font-italic">Schlendern Sie durch Freiburgs historische Altstadt.</p>
       </div>
     </div>
   </div>
